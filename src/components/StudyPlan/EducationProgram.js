23fa4dc15ea696bb0c3a7bdf6021d09b325fac1f
LingDev <linhdeptrai1029i@gmail.com>
2020-12-29 01:06:55 +0700

    wip:future/subject

--- src/components/StudyPlan/EducationProgram.js
+++ src/components/StudyPlan/EducationProgram.js
@@ -1,9 +1,35 @@
-import React from "react";
-
+import React, {useEffect, useState} from "react";
+import {Modal} from 'antd';
 const EducationProgram = (props) => {
-    return(
-        <></>
-    );
+    const [isModalVisible, setIsModalVisible] = useState(false);
+
+    const showModal = () => {
+      setIsModalVisible(true);
+    };
+  
+    const handleOk = () => {
+      setIsModalVisible(false);
+    };
+  
+    const handleCancel = () => {
+      setIsModalVisible(false);
+    };
+  
+    useEffect(()=>{
+        setIsModalVisible(props.isModalVisible);
+    },[props.isModalVisible])
+    
+    return (
+      <>
+        <Button type="primary" onClick={showModal}>
+          Open Modal
+        </Button>
+        <Modal title="Basic Modal" visible={isModalVisible} onOk={handleOk} onCancel={handleCancel}>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+          <p>Some contents...</p>
+        </Modal>
+      </>
 };
 
 export default EducationProgram;
